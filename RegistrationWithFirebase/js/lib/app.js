6df9a2ede4ba2ad9acaa17c3783e5c5a8bf3b2a1
Bruno Sansigolo <bruno@sansigolo.com>
2018-09-11 22:26:45 +0100

    registration with firebase

---
+++ RegistrationWithFirebase/js/lib/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module('myApp', ['ngRoute', 'firebase']);
+
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+    .when('/login', {
+        templateUrl: '/views/login.html',
+        controller: 'RegistrationController'
+    })
+    .when('/register', {
+        templateUrl: '/views/register.html',
+        controller: 'RegistrationController'
+    })
+    .when('/success', {
+        templateUrl: '/views/success.html',
+        controller: 'SuccessController'
+    })
+    .otherwise({
+        redirectTo: '/login'
+    })
+}]);
---
+++ RegistrationWithFirebase/js/services/authentication.js
@@ -0,0 +1,28 @@
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', function($rootScope, $firebaseAuth) {
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+    return {
+        login: function(user) {
+                $rootScope.message = "Welcome " + $scope.user.email;
+        },//login
+        register: function(user) {
+                auth.$createUserWithEmailAndPassword(
+                    user.email,
+                    user.password
+                ).then(function(firebaseUser) {
+                    console.log(regUser);
+                    var regRef = ref.child('users')
+                    .child(regUser.uid).set({
+                        date: firebase.database.ServerValue.TIMESTAMP,
+                        regUser: regUser.uid,
+                        firstname: user.firstname,
+                        lastname: user.lastname,
+                        email: user.email
+                    });//userinfo
+                    $rootScope.message = "Hi " + user.firstname + ", Thanks for registering";
+                }).catch(function(error) {
+                    $rootScope.message = error.message;
+                }); //createUserWithEmailAndPassword
+        }//register
+    }//return
+}]);//factory
